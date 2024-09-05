cb155a32d5bb4e296fb6dc558f46de75951dbd61
Dan <d.y.tkachenko@gmail.com>
2024-09-05 15:11:51 +0200

    Added base security config

--- application/src/main/java/pl/lawit/application/security/SecurityConfig.java
+++ application/src/main/java/pl/lawit/application/security/SecurityConfig.java
@@ -1,38 +1,92 @@
 package pl.lawit.application.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import pl.lawit.application.configuration.SecurityProperties;
 import pl.lawit.idp.firebase.configuration.FirebaseAuthenticationProvider;
+import pl.lawit.kernel.model.ApplicationUserRole;
 
 import java.util.Collections;
 
-@EnableWebSecurity
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)  // Disable CSRF protection for simplicity; adjust as needed
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/**").permitAll()  // Permit all requests to /api/users endpoints
-                        .anyRequest().authenticated()  // Require authentication for all other requests
-                )
-        ;  // Use HTTP Basic Authentication; adjust as needed
+	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+	private String issuerUri;
 
-        return http.build();
-    }
+	private final JwtClaimResolver jwtClaimResolver;
 
-    @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
-        FirebaseAuthenticationProvider provider = new FirebaseAuthenticationProvider(userDetailsService);
-        return new ProviderManager(Collections.singletonList(provider));
-    }
+	private final SecurityProperties securityProperties;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests($ ->
+				$.requestMatchers(getPermittedMatchers()).permitAll()
+					.anyRequest()
+					.authenticated())
+			.cors(withDefaults())
+			.csrf(AbstractHttpConfigurer::disable)
+			.oauth2ResourceServer($ -> $.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+
+		return http.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+		FirebaseAuthenticationProvider provider = new FirebaseAuthenticationProvider(userDetailsService);
+		return new ProviderManager(Collections.singletonList(provider));
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuerUri);
+
+		OAuth2TokenValidator<Jwt> issuerValidator = JwtValidators.createDefaultWithIssuer(issuerUri);
+
+		OAuth2TokenValidator<Jwt> delegate = new DelegatingOAuth2TokenValidator<>(issuerValidator);
+
+		jwtDecoder.setJwtValidator(delegate);
+
+		return jwtDecoder;
+	}
+
+	private String[] getPermittedMatchers() {
+		return securityProperties.getPermittedMatchers().toArray(String[]::new);
+	}
+
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> jwtClaimResolver.getUserRoles(jwt)
+			.map(this::mapToGrantedAuthority)
+			.toJavaList());
+		return jwtAuthenticationConverter;
+	}
+
+	private GrantedAuthority mapToGrantedAuthority(ApplicationUserRole userGroup) {
+		return new SimpleGrantedAuthority("ROLE_" + userGroup.name());
+	}
 }
